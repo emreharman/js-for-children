a24850b39113d4e10aeb0f68275e57a3add5bc47
emreharman <emre.harman@ogr.sakarya.edu.tr>
2021-09-17 22:36:18 +0300

    dersteki uygulamalar eklendi

--- script.js
+++ script.js
@@ -45,8 +45,9 @@ if (sayi < 0) {
     alert(`${sayi} sayısının faktoriyeli=${sonuc}`);
 }
 
-
-//dersteki örnek
+////////////////////////////////
+//dersteki örnek 9-13
+//////////////////////////////
 
 let sayi1 = Number(prompt("Lütfen bir sayı girin"))
 
@@ -62,8 +63,93 @@ function faktoriyelAl(sayi) {
     return sonuc
 }
 
+////////////////////////////////////////
+// dersteki örnek 13-16
+////////////////////////////////////////7
 
 
+//değişkenler (variable)
+
+//Veri Tipleri data types
+//string -> metinsel ifadeler
+//number -> 10,-10 (integer),3.7 (float)
+//boolean -> true,false
+
+//bilgi verme console.log(),alert(),
+//kullanıcıdan bilgi alma prompt()->bilgiyi string alır
+
+//temel matematiksel işlem operatörleri
+// +,-,/,*,%,** 
+
+//karşılaştırma operatörleri
+//<,>,<=,>=, ==, !=
+//js için-> === Tip ve  değer karşılaştırması yapar
+
+//conditions if statements
+/*let not1 = Number(prompt("1. notu girin"))
+let not2 = Number(prompt("2. notu girin"))
+
+
+
+if (ortalamaBul(not1,not2) < 50) {
+    alert("üzgünüm dersten kaldınız. \nortalamanız="+ ortalamaBul(not1,not2))
+}
+if (ortalamaBul(not1,not2) >= 50 && ortalamaBul(not1,not2)<= 60) {
+    alert("geçtiniz ama biraz daha gayret ortalamanız="+ ortalamaBul(not1,not2))
+}
+if (ortalamaBul(not1,not2) > 60) {
+    alert("tebrikler notunuz güzel ortalamanız="+ ortalamaBul(not1,not2))
+}
+
+function ortalamaBul(not1, not2) {
+    let ortalama = (not1 + not2) / 2
+    return ortalama
+}*/
+
+//döngüler (loops)
+//for,while,do while
+
+/*let sayi = Number(prompt("Sayi giriniz"))
+
+let sonuc = 0
+
+/*for (let sayac = 0; sayac <= sayi; sayac++){
+    sonuc=sonuc + sayac
+}*/
+
+/*let sayac=0
+while (true) {
+    sonuc = sonuc + sayac
+    if (sayac === sayi) {
+        break
+    }
+    sayac++
+}
+
+alert(sonuc)*/
+
+
+
+let sayi = Number(prompt("Sayı girin"))
+
+alert(faktoriyelBul(sayi))
+
+function faktoriyelBul(sayi) {
+    if (isNaN(sayi)) {
+        return "Sayı girmediniz"
+    }
+    let sonuc = 1
+    if (sayi === 0) {
+        return 1
+    }
+    if (sayi < 0) {
+        return "negatif sayıların faktöriyeli yoktur"
+    }
+    for (let sayac = sayi; sayac > 0; sayac--){
+        sonuc = sonuc * sayac
+    }
+    return sonuc
+}
 
 
 
